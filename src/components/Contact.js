ceed4459fa7c9cd99af0d0836e3db91d91957c22
Ryan Cheung <ryan_cheung@hotmail.co.uk>
2023-03-17 18:00:40 +0000

    Contact form added to Contact component. More CSS added for hover on nav bar etc

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -34,6 +34,33 @@ function Contact() {
             </li>
           </div>
         </div>
+    </div>
+    <div id="contact-form" className="container mt-5">
+        <div className= "row g-3">
+            <div className="col-md-6">
+                <label htmlFor="firstName" className="form-label">First Name</label>
+                <input type="text" className="form-control" id="firstName"></input>
+            </div>
+            <div className="col-md-6">
+                <label htmlFor="lastName" className="form-label">Last Name</label>
+                <input type="text" className="form-control" id="lastName"></input>
+            </div>
+            <div className="col-md-8">
+                <label htmlFor="email" className="form-label">E-mail</label>
+                <input type="text" className="form-control" id="email"></input>
+            </div>
+            <div className="col-md-4">
+                <label htmlFor="phone" className="form-label">Phone Number</label>
+                <input type="text" className="form-control" id="phone"></input>
+            </div>
+            <div className="col-md-12">
+            <label htmlFor="comments" className="form-label">Comments</label>
+            <textarea className="form-control" id="comments" rows="3"></textarea>
+            </div>
+            <div className="col-md-12">
+                <button type="submit" className="btn btn=primary"> Submit</button>
+            </div>
+        </div>
       </div>
     </div>
   );
--- src/components/ProjectsPage.js
+++ src/components/ProjectsPage.js
@@ -4,20 +4,16 @@ import Projects from "../components/projects";
 
 function ProjectsPage() {
   return (
-    <div className="container" id="work">
+    <div id="work">
       <h1 className = "text-center title">Work Gallery</h1>
       <div className="container text-center">
         <div className="row">
           <div className="col-sm-2 col-md-2 col-lg-2"></div>
-          <div className="col-sm-8 col-md-8 col-lg-8">
-            <div className="row">
-              <div className="col-12">
+              <div className="cardstyle col-sm-8 col-md-8 col-lg-8">
                 {gallery.map((CurrentObject) => (
                   <Projects props={CurrentObject} key={CurrentObject.id}></Projects>
                 ))}
               </div>
-            </div>
-          </div>
           <div className="col-sm-2 col-md-2 col-lg-2"></div>
         </div>
       </div>
